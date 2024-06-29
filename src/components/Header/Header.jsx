dce1cf6ef463dadec143ca8faf92a4d8ef257094
sowmik-sec <sowmik.sec@gmail.com>
2024-06-29 20:40:51 +0600

    reduce code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,22 @@
 import { NavLink } from "react-router-dom";
 
 function Header() {
+  const links = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/statistics"}>Statistics</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/applied-jobs"}>Applied Jobs</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/blog"}>Blog</NavLink>
+      </li>
+    </>
+  );
   return (
     <nav className="navbar bg-base-100">
       <div className="navbar-start">
@@ -25,31 +41,13 @@ function Header() {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <NavLink to={"/statistics"}>Statistics</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/applied-jobs"}>Applied Jobs</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/blog"}>Blog</NavLink>
-            </li>
+            {links}
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">CareerHub</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <NavLink to={"/statistics"}>Statistics</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/applied-jobs"}>Applied Jobs</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/blog"}>Blog</NavLink>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
         <a className="btn">Start Applying</a>
