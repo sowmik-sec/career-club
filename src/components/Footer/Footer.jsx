cb2c81d9c44fcc2ac100e53df39e9e3d7e320e86
sowmik-sec <sowmik.sec@gmail.com>
2024-06-29 20:34:14 +0600

    create footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,52 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer bg-base-200 text-base-content p-10">
+      <nav className="max-w-56">
+        <h2 className="text-3xl">CareerHub</h2>
+        <p>
+          There are many variations of passages of Lorem Ipsum , but the
+          majority have suffered alteration in some form.
+        </p>
+        <img className="w-28 h-10" src="../../assets/icons/social.png" alt="" />
+      </nav>
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+      <form>
+        <h6 className="footer-title">Newsletter</h6>
+        <fieldset className="form-control w-80">
+          <label className="label">
+            <span className="label-text">Enter your email address</span>
+          </label>
+          <div className="join">
+            <input
+              type="text"
+              placeholder="username@site.com"
+              className="input input-bordered join-item"
+            />
+            <button className="btn btn-primary join-item">Subscribe</button>
+          </div>
+        </fieldset>
+      </form>
+    </footer>
+  );
 }
 
 export default Footer;
