dea6739d477d35d30ebbd862b8a381506a46db87
sowmik-sec <sowmik.sec@gmail.com>
2024-06-30 12:43:10 +0600

    conditionally set classname

--- src/components/FeaturedJob/FeaturedJob.jsx
+++ src/components/FeaturedJob/FeaturedJob.jsx
@@ -1,5 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import ButtonPrimary from "../ButtonPrimary/ButtonPrimary";
+import locationImg from "../../assets/icons/location2.png";
+import moneyImg from "../../assets/icons/money.png";
 
 function FeaturedJob({ job }) {
   const {
@@ -11,26 +13,38 @@ function FeaturedJob({ job }) {
     job_type,
     salary,
   } = job;
+  const curLocation = useLocation();
+  console.log("location", curLocation);
   return (
-    <div className="mx-auto space-y-6 border border-indigo-400 p-10 w-full rounded-md">
-      <img src={logo} alt="" />
-      <h5 className="text-2xl">{job_title}</h5>
-      <h6 className="text-xl">{company_name}</h6>
+    <div
+      className={`mx-auto  border border-indigo-400 p-10 w-full rounded-md ${
+        curLocation.pathname === "/" ? "" : "flex items-center justify-between"
+      }`}
+    >
+      <div className="space-y-6">
+        <img src={logo} alt="" />
+        <h5 className="text-2xl">{job_title}</h5>
+        <h6 className="text-xl">{company_name}</h6>
+        <div>
+          <span className="px-5 py-2 border border-violet-400 rounded-md mr-5">
+            {remote_or_onsite}
+          </span>
+          <span className="px-5 py-2 border border-violet-400 rounded-md">
+            {job_type}
+          </span>
+        </div>
+        <div className="flex">
+          <img src={locationImg} alt="" />
+          <p className="mr-8">{location}</p>
+          <img src={moneyImg} alt="" />
+          <p>{salary}</p>
+        </div>
+      </div>
       <div>
-        <span className="px-5 py-2 border border-violet-400 rounded-md mr-5">
-          {remote_or_onsite}
-        </span>
-        <span className="px-5 py-2 border border-violet-400 rounded-md">
-          {job_type}
-        </span>
+        <Link>
+          <ButtonPrimary padding={"px-4 py-2 mt-3"}>View Details</ButtonPrimary>
+        </Link>
       </div>
-      <div className="flex">
-        <p className="mr-8">{location}</p>
-        <p>{salary}</p>
-      </div>
-      <Link>
-        <ButtonPrimary padding={"px-4 py-2"}>View Details</ButtonPrimary>
-      </Link>
     </div>
   );
 }
