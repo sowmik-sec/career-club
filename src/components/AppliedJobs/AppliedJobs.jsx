2eedd299c7641fb41773ccc4ff772cf44bc35366
sowmik-sec <sowmik.sec@gmail.com>
2024-06-30 12:43:29 +0600

    complete applied jobs section

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,7 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import FeaturedJob from "../FeaturedJob/FeaturedJob";
+
 function AppliedJobs() {
+  const jobs = useLoaderData() || [];
   return (
-    <div>
-      <h2>AppliedJobs</h2>
+    <div className="container mx-auto">
+      <div className="flex items-center justify-center h-36">
+        <h2>Applied Jobs</h2>
+      </div>
+      <div className="flex flex-col gap-6 mb-24">
+        {jobs.map((job) => (
+          <FeaturedJob key={job.id} job={job} />
+        ))}
+      </div>
     </div>
   );
 }
