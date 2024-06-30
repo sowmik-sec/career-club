82ad653c32677867977a0cc5c5665cc1d7eda885
sowmik-sec <sowmik.sec@gmail.com>
2024-06-30 15:13:59 +0600

    add event handler to button

--- src/components/ButtonPrimary/ButtonPrimary.jsx
+++ src/components/ButtonPrimary/ButtonPrimary.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
-function ButtonPrimary({ padding, children }) {
+function ButtonPrimary({ onClick = undefined, padding, children }) {
   return (
     <button
       className={`bg-gradient-to-r from-indigo-400 to-indigo-600 text-white ${padding} rounded-lg`}
+      onClick={onClick}
     >
       {children}
     </button>
